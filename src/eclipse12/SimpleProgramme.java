feccbd9a8d43207d8cea6b863a4aab89ecd45b01
DELL <DELL@DESKTOP-PTBT4NO>
2022-04-23 16:19:56 +0530

    local

--- src/eclipse12/SimpleProgramme.java
+++ src/eclipse12/SimpleProgramme.java
@@ -4,7 +4,13 @@ public class SimpleProgramme {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("hello world");
+		int a = 10;
+		
+		int b = 20;
+		
+		int c = a+b;
+		
+		System.out.println(c);
 	}
 
 }
