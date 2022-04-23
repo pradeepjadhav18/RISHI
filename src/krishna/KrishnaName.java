4b28fcf0deb4ccc3670c97b45d0fc3488dc9d854
DELL <DELL@DESKTOP-PTBT4NO>
2022-04-23 15:13:32 +0530

    hiii

---
+++ src/krishna/KrishnaName.java
@@ -0,0 +1,17 @@
+package krishna;
+
+public class KrishnaName {
+	
+	public static void main(String[] args) {
+		
+	System.out.println("Pradeep ");	
+	
+	System.out.println();
+	
+	System.out.println("Jadhav");
+		
+		
+	}
+	
+
+}
